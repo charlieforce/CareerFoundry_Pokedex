d437e95af33555a9249321b5edda9dffd5676622
Charlie KB <charlieforce1999@gmail.com>
2019-08-14 15:17:03 -0400

    This is the latest commit for assignment 4 work
    This is a commit for work added to assignment 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,34 @@
-alert('Hello World');
+var repository =
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Charizard',
+    height: 1.7,
+    types: ['fire', 'flying']
+  },
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    types: ['water']
+  }
+
+Object.keys(repository).forEach(function(property){
+  console.log(repository[property]);
+});
+
+
+// printArrayDetails function declaration
+//function printArrayDetails(){
+  for (var i = 0; i < repository.length; i++){
+    // document.write("<p>" + repository[i].name + "</p>");
+    // printing repository[i]’s other details
+    // ...
+  }
+}
+
+// Calling printArrayDetails function twice
+printArrayDetails();
+printArrayDetails();
